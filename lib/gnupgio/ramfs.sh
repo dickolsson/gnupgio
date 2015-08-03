dcecea04c5d4a616d3536cf43de4dc42e6713bb6
dickolsson <dick.olsson@senzilla.com>
2015-08-03 14:56:05 +0100

    Provide an implementation of mkramfs for Darwin systems

--- lib/gnupgio/ramfs.sh
+++ lib/gnupgio/ramfs.sh
@@ -6,10 +6,16 @@
 #
 mkramfs() {
   # Create temporary mount point
-  mnt=$(mktemp -d)
+  mnt=$(mktemp -d tmp.XXXXXXXX)
   # Mount temporary file system
   if [ "$(uname)" = "Linux" ]; then
     sudo mount -t ramfs -o mode=777 ramfs $mnt
+  elif [ "$(uname)" = "Darwin" ]; then
+    ramfs_size_sectors=$((10*1024*1024/512))
+    ramdisk_dev=`hdid -nomount ram://${ramfs_size_sectors}`
+
+    newfs_hfs -v 'ram disk' ${ramdisk_dev} > /dev/null 2>&1
+    mount -o noatime -t hfs ${ramdisk_dev} ${mnt}
   fi
   # Return the mount point
   echo $mnt
@@ -23,6 +29,8 @@ rmramfs() {
   # Unmount temporary file system
   if [ "$(uname)" = "Linux" ]; then
     sudo umount $1
+  elif [ "$(uname)" = "Darwin" ]; then
+    umount $1
   fi
   # Remove temporary mount point
   rm -rf $1
